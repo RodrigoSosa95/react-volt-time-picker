e7bd96ca5dc5c8728d145168d47a6f50f85e77d5
Rodrigo Sosa <RodrigoSosa95@live.com.mx>
2017-06-11 23:43:26 -0500

    Added styles

--- example/src/example.js
+++ example/src/example.js
@@ -1,12 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var moment = require('moment');
 var VoltTimePicker = require('react-volt-time-picker');
 
 var App = React.createClass({
+	getInitialState: function() {
+		return {
+			value: moment().local().startOf('d').add(9, 'h').toString()
+		};
+	},
+	handleHourChange: function(hour) {
+		this.setState({value: hour});
+	},
 	render () {
 		return (
 			<div>
-				<VoltTimePicker />
+				<VoltTimePicker
+					value={this.state.value}
+					start={moment().local().startOf('d').add(9, 'h')}
+					end={moment().local().startOf('d').add(21, 'h')}
+					onChange={value => this.handleHourChange(value)}
+				/>
 			</div>
 		);
 	}
--- src/VoltTimePicker.js
+++ src/VoltTimePicker.js
@@ -1,9 +1,92 @@
 var React = require('react');
+var moment = require('moment');
+var uuid = require('uuid');
 
 var VoltTimePicker = React.createClass({
-	render () {
-		return <div>Chinga tu madre</div>;
-	}
+  propTypes: {
+    className: React.PropTypes.string,
+    end: React.PropTypes.objectOf(moment).isRequired,
+    format: React.PropTypes.string,
+    label: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.func,
+    start: React.PropTypes.objectOf(moment).isRequired,
+    steps: React.PropTypes.number,
+    value: React.PropTypes.string.isRequired,
+  },
+  getDefaultProps: function() {
+    return {
+      className: '',
+      format: 'hh:mm A',
+      label: 'Time',
+      onChange: time => console.log(time),
+      steps: 30,
+    };
+  },
+  renderSteps: function() {
+    const { start, steps, end, format } = this.props;
+    const difference = end.diff(start, 'minutes');
+    const slots = difference / steps;
+    const hours = [start.toString()];
+    const options = [];
+    for (var i = 0; i < slots; i++) {
+      hours.push(start.add(steps, 'm').toString());
+    }
+    hours.map(hour => (
+      options.push(
+        <option key={uuid()} value={moment(hour)}>
+          {moment(hour).format(format)}
+        </option>
+      )
+    ));
+    return options;
+  },
+  render () {
+    return (
+      <form className={this.props.className}>
+        <label
+          htmlFor="volt-time-picker"
+          style={styles.timePickerLabel}
+        >
+          {this.props.label}
+        </label>
+        <div style={styles.selectWrapper} className={`${this.props.className}`}>
+          <select
+            style={styles.timePickerSelect}
+            name="volt-time-picker"
+            value={this.props.value}
+            onChange={event => this.props.onChange(event.target.value)}
+          >
+            {this.renderSteps()}
+          </select>
+        </div>
+      </form>
+    );
+  }
 });
+
+const styles = {
+  timePickerLabel: {
+    display: 'block',
+    marginBottom: '0.3rem'
+  },
+  timePickerSelect: {
+    display: 'block',
+    fontSize: '1rem',
+    width: '100%',
+    appearance: 'none',
+    autofill: '#fff !important',
+    backgroundColor: 'transparent',
+    borderRadius: 0,
+    border: 'none',
+  },
+  selectWrapper: {
+    width: '150px',
+    height: '30px',
+    display: 'flex',
+    borderRadius: '10px',
+    backgroundColor: 'whitesmoke',
+    justifyContent: 'center',
+  }
+};
 
 export default VoltTimePicker;
