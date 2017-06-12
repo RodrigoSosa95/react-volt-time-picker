21da5f58e76c41cee5a65f647f70ba622617bb7c
Rodrigo Sosa <RodrigoSosa95@live.com.mx>
2017-06-12 00:02:57 -0500

    initial version

--- example/src/example.js
+++ example/src/example.js
@@ -4,26 +4,26 @@ var moment = require('moment');
 var VoltTimePicker = require('react-volt-time-picker');
 
 var App = React.createClass({
-	getInitialState: function() {
-		return {
-			value: moment().local().startOf('d').add(9, 'h').toString()
-		};
-	},
-	handleHourChange: function(hour) {
-		this.setState({value: hour});
-	},
-	render () {
-		return (
-			<div>
-				<VoltTimePicker
-					value={this.state.value}
-					start={moment().local().startOf('d').add(9, 'h')}
-					end={moment().local().startOf('d').add(21, 'h')}
-					onChange={value => this.handleHourChange(value)}
-				/>
-			</div>
-		);
-	}
+  getInitialState: function() {
+    return {
+      value: moment().local().startOf('d').add(9, 'h').toString()
+    };
+  },
+  handleHourChange: function(hour) {
+    this.setState({value: hour});
+  },
+  render () {
+    return (
+      <div>
+        <VoltTimePicker
+          value={this.state.value}
+          start={moment().local().startOf('d').add(9, 'h')}
+          end={moment().local().startOf('d').add(21, 'h')}
+          onChange={value => this.handleHourChange(value)}
+        />
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<App />, document.getElementById('app'));
